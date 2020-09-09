7b8c29a4625da9eea6a826eefc1cd2714b0328f1
Lightsource-Pris <israelakinakinsanya@gmail.com>
2020-09-09 21:28:32 +0100

    Updates

---
+++ precache-manifest.df960b6916cdb610feb28ded451bae59.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07206b7d9006d7bc4669bf31b8d7e7e8",
+    "url": "/shallom-todo/index.html"
+  },
+  {
+    "revision": "fa9c0697b9c34b7a17d2",
+    "url": "/shallom-todo/static/js/2.0db6bc1c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shallom-todo/static/js/2.0db6bc1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1d1658be161187ec318",
+    "url": "/shallom-todo/static/js/main.735c7cce.chunk.js"
+  },
+  {
+    "revision": "b90e9de3e61b471d640b",
+    "url": "/shallom-todo/static/js/runtime-main.e1d86fe7.js"
+  }
+]);
